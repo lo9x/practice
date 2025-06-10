e328d7d15f03cda7cdfb091d34ed6ce2ae3da03a
lo9x <ilyasnor59757@gmail.com>
2025-06-11 02:06:27 +0400

    ;

--- App.vue
+++ App.vue
@@ -27,47 +27,87 @@
         <div class="dota2">
         </div>
       </div>
+
       <div class="flex_hero">
         <div class="hero">
           <img src="@/assets/image (12).png" width="428px" height="571px">
+          <label>
+            Георгий Прокофьевич Хлебородов
+          </label>
         </div>
-      <div class="heroes">
-        <div class="hero">
-          <img src="@/assets/image.png" width="208px" height="277px">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image-1.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
-        <div class="hero">
-          <img src="@/assets/image.png">
-        </div>
+        <div class="heroes">
+          <div class="hero">
+            <img src="@/assets/image.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-1.png">
+            <label>
+              Виктор Владимирович Антроповский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-2.png">
+            <label>
+              Анна Тимуровна Матюшечкина
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-3.png">
+            <label>
+              Егор Тимурович Ващекин
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-4.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-5.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-6.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-7.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-8.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-9.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-10.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
+          <div class="hero">
+            <img src="@/assets/image-11.png">
+            <label>
+              Александр Александрович Константинопольский
+            </label>
+          </div>
         </div>
       </div>
     </div>
@@ -90,29 +130,28 @@ body{
   overflow: hidden;
   margin: 80px 0px 80px 80px;
 }
+
 header{
 display: flex;
 flex-direction: row;
 align-items: center;
 }
+
 header p{
   font-size: 3em;
 }
+
 .logo{
   margin-right: 44px;
 }
-.main{ 
- /* display: flex; 
-  flex-direction: row;
-  align-items: center;  */
 
-}
 .hoho{
   display: flex;
   flex-direction: row;
   align-items: center;
   width: 100%;
 }
+
 .search{
   cursor: pointer;
   border: 0 ;
@@ -125,8 +164,8 @@ header p{
   align-items: center;
   font-size: 1em;
   color:white;
-  
 }
+
 .filter{
   border: 1px solid;
   cursor: pointer;
@@ -140,14 +179,17 @@ header p{
   height: 69px;
   margin: 0 16px;
 }
+
 .hohp{
   font-size: 3em;
   color: rgb(114, 114, 114);
 }
+
 .dota2{
 border: 2px dotted #000; 
 flex: 1;
 }
+
 .heroes{
   display: grid;
   grid-template-columns: repeat(6,1fr);
@@ -155,10 +197,31 @@ flex: 1;
   margin-left: 10px;
   
 }
+
 .flex_hero{
   display: flex;
   flex-direction: row;
-  align-items: center;
- 
+  align-items: center; 
+}
+
+.hero{
+  position: relative;
+  margin: 0px;
+}
+
+.hero img{
+  filter: sepia(70%);
+}
+
+.hero label{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  color: white;
+  padding: 12px 15px;
+  font-size: 16px;
+  line-height: 1.3;
+  box-sizing: border-box;
 }
 </style>
